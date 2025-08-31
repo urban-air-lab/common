024517bfb3a872141c455a63a73636974983e879
Tobias Weiland <post@tobias-weiland.eu>
2025-08-31 17:45:11 +0200

    implement common logger

---
+++ ual/logging.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+
+def get_logger(name: str = None):
+    logger = logging.getLogger(name)
+
+    if not logger.handlers:
+        logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        logger.propagate = False
+
+    return logger
