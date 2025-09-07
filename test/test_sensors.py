65be9faeda0c046792d03c23f6fc483366cc0112
Tobias Weiland <post@tobias-weiland.eu>
2025-09-07 17:38:28 +0200

    fix test

--- test/test_sensors.py
+++ test/test_sensors.py
@@ -4,7 +4,7 @@ from ual.influx.sensors import SensorSource
 
 
 def test_sensor_source():
-    sensor_source = SensorSource(bucket=InfluxBuckets.UAL_MINUTE_CALIBRATION_BUCKET.value,
-                                 sensor=sensors.UALSensors.UAL_3.value)
-    assert sensor_source.bucket == "ual-minute-calibration"
-    assert sensor_source.sensor == "ual-3"
+    sensor_source = SensorSource(bucket=InfluxBuckets.UAL_MINUTE_CALIBRATION_BUCKET,
+                                 sensor=sensors.UALSensors.UAL_3)
+    assert sensor_source.get_bucket() == "ual-minute-calibration"
+    assert sensor_source.get_sensor() == "ual-3"
